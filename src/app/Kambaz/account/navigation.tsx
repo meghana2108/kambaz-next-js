b2ca143658e07c1907dc81ff7453988e3062901f
Vatsa <ssrivatsajobs@gmail.com>
2025-09-22 14:14:31 -0400

    Changes made in account

--- src/app/Kambaz/account/navigation.tsx
+++ src/app/Kambaz/account/navigation.tsx
@@ -3,9 +3,9 @@ export default function Accountnavigation() {
     return (
         <div>
             <ul>
-                <li><Link href="signin">Sign in</Link></li>
-                <li><Link href="signup">Sign up</Link></li>
-                <li><Link href="profile">Profile</Link></li>
+                <li><Link href="/kambaz/account/signin">Sign in</Link></li>
+                <li><Link href="/kambaz/account/signup">Sign up</Link></li>
+                <li><Link href="/kambaz/account/profile">Profile</Link></li>
             </ul>
         </div>
     );
