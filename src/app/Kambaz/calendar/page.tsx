a93efa684ea2811cdc44ff0734a9e23f29e0e0bd
Vatsa <ssrivatsajobs@gmail.com>
2025-09-22 19:03:20 -0400

    Commit 3

--- src/app/Kambaz/calendar/page.tsx
+++ src/app/Kambaz/calendar/page.tsx
@@ -2,6 +2,7 @@ export default function Calendar() {
     return (
         <div id="wd-calendar">
             <h1>Calendar:</h1>
+            <p>This is a static page which lets us to do changes to Calendar section here.</p>
         </div>
     );
 }
