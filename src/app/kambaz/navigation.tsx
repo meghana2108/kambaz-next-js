c4b0a1a3418268ab80d577a12da3c6c8b20af634
Vatsa <ssrivatsajobs@gmail.com>
2025-10-20 13:16:04 -0400

    Made changes

--- src/app/kambaz/navigation.tsx
+++ src/app/kambaz/navigation.tsx
@@ -6,19 +6,21 @@ import { FaCalendarAlt } from "react-icons/fa";
 import { HiOutlineInbox } from "react-icons/hi";
 import { ImLab } from "react-icons/im";
 import { ListGroupItem, ListGroup } from "react-bootstrap";
-import { usePathname } from "next/navigation";
+import { usePathname, useParams } from "next/navigation";
+import courses from "@/app/kambaz/Database/courses.json";
 import Image from "next/image";
 import "./index.css";
 import Link from "next/link";
 export default function Kambaznavigation() {
     const pathname = usePathname();
+    const params = useParams();
+    const currentCourseId = (params.courseId as string) || courses[0]._id;
     const links = [
         {label: "Dashboard", path:"/kambaz/Dashboard", icon: TfiDashboard},
-        {label: "Courses", path:"/kambaz/Dashboard", icon: FaBook},
+        {label: "Courses", path:`/kambaz/Courses/${currentCourseId}/home`, icon: FaBook},
         {label: "Calendar", path:"/kambaz/Calendar", icon: FaCalendarAlt},
         {label: "Inbox", path:"/kambaz/Inbox", icon: HiOutlineInbox},
         {label: "Labs", path:"/Labs", icon: ImLab},
-
     ]
     return (
             <ListGroup id="wd-kambaz-navigation" style={{width: 110}}
@@ -26,7 +28,7 @@ export default function Kambaznavigation() {
             <ListGroupItem id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/"
                 action className="bg-black border-0 text-center">
                 <Image src="/images/neu.png" width={75} height={75} alt="logo"/></ListGroupItem>
-            <ListGroupItem as={Link} href="/kambaz/Account/profile" className={`text-center border-0 bg-black ${pathname.includes("Account")?"bg-white text-danger":"bg-black text-white"}`}>
+            <ListGroupItem as={Link} href="/kambaz/Account/profile" className={`text-center border-0 bg-black ${pathname.includes("Account")?"bg-white      text-danger":"bg-black text-white"}`}>
                 <MdAccountCircle className={`fs-3 ${pathname.includes("Account")?"text-danger":"text-white"}`}/> <br/>
                 Account
             </ListGroupItem>
