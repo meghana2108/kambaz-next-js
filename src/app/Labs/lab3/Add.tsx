1f4514dcb4a2ff4ab68f6c58142fadd1e28553cc
Vatsa <ssrivatsajobs@gmail.com>
2025-10-20 21:35:36 -0400

    One final commit

--- src/app/Labs/lab3/Add.tsx
+++ src/app/Labs/lab3/Add.tsx
@@ -1,7 +1,7 @@
 export default function Add ({a,b}:{a:number, b:number}) {
     return (
         <div id="wd-add">
-            <h3>ADD</h3>
+            <h3>ADD (Parameterizing Components)</h3>
             a = {a} <br/>
             b = {b} <br/>
             a + b = {a+b} <hr/>
--- src/app/Labs/lab3/Square.tsx
+++ src/app/Labs/lab3/Square.tsx
@@ -3,7 +3,7 @@ export default function Square ({children} : {children:ReactNode}) {
     const num = Number(children);
     return (
         <div>
-        <h3>Square of a number</h3>
+        <h3>Square of a number (Child Components)</h3>
         Square of {num} = <span id="wd-square">{num*num}</span> <hr/>
         </div>
     );
