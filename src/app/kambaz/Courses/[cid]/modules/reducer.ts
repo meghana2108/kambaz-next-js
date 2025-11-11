3a581b6b51497850e74615617426551670f41565
Vatsa <ssrivatsajobs@gmail.com>
2025-11-10 19:27:48 -0500

    A4 7th commit

--- src/app/kambaz/Courses/[cid]/modules/reducer.ts
+++ src/app/kambaz/Courses/[cid]/modules/reducer.ts
@@ -1,39 +1,60 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-import {createSlice} from "@reduxjs/toolkit";
-import modules from "../../../Database/modules.json";
-import {v4 as uuidv4} from "uuid";
-const initialState = {
-    modules: modules,
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import modulesData from "../../../Database/modules.json";
+import { v4 as uuidv4 } from "uuid";
+interface Lesson {
+  _id: string;
+  name: string;
+  description: string;
+  module: string;
 }
-const moduleSlice = createSlice ({
-    name: "modules",
-    initialState,
-    reducers: {
-        addModule: (state, {payload:module}) => {
-            const newModule: any = {
-                _id: uuidv4,
-                lessons: [],
-                name: module.name,
-                course: module.course
-            };
-            state.modules = [...state.modules, newModule] as any;
-        },
-         deleteModule: (state, { payload: moduleId }) => {
-      state.modules = state.modules.filter(
-        (m: any) => m._id !== moduleId);
+
+interface Module {
+  _id: string;
+  name: string;
+  description: string;
+  course: string;
+  lessons: Lesson[];
+  editing?: boolean; 
+}
+
+interface ModulesState {
+  modules: Module[];
+}
+
+const initialState: ModulesState = {
+  modules: modulesData as Module[],
+};
+
+const moduleSlice = createSlice({
+  name: "modules",
+  initialState,
+  reducers: {
+    addModule: (state, { payload: module }: PayloadAction<{ name: string; course: string }>) => {
+      const newModule: Module = {
+        _id: uuidv4(),
+        lessons: [],
+        name: module.name,
+        description: "",
+        course: module.course,
+      };
+      state.modules = [...state.modules, newModule];
     },
-    updateModule: (state, { payload: module }) => {
-      state.modules = state.modules.map((m: any) =>
+    deleteModule: (state, { payload: moduleId }: PayloadAction<string>) => {
+      state.modules = state.modules.filter((m) => m._id !== moduleId);
+    },
+    updateModule: (state, { payload: module }: PayloadAction<Module>) => {
+      state.modules = state.modules.map((m) =>
         m._id === module._id ? module : m
-      ) as any;
+      );
     },
-    editModule: (state, { payload: moduleId }) => {
-      state.modules = state.modules.map((m: any) =>
+    editModule: (state, { payload: moduleId }: PayloadAction<string>) => {
+      state.modules = state.modules.map((m) =>
         m._id === moduleId ? { ...m, editing: true } : m
-      ) as any;
+      );
     },
-    },
+  },
 });
+
 export const { addModule, deleteModule, updateModule, editModule } = moduleSlice.actions;
 export default moduleSlice.reducer;
